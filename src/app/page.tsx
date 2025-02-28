55eb9e494fc0c243b33a5d607b1685f21d1dbac1
dane168 <danieljevans168@gmail.com>
2025-02-28 21:21:11 +0000

    added some logos. needs to be colorful. Too dull right now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Header from "@/components/Header";
 import EducationItem from "../components/education_item";
 import { promises as fs } from 'fs';
+import { FaHtml5, FaPython } from "react-icons/fa";
+import { DiMsqlServer } from "react-icons/di";
+import { SiTypescript } from "react-icons/si";
 
 export type EducationData = {
   school: string;
@@ -16,37 +19,54 @@ export default async function Home() {
 
   return (
     <>
-      <div className="h-screen md:text-md bg-gradient-to-br from-blue-900 to-blue-300 dark:bg-gradient-to-r dark:from-black dark:to-slate-950">
+      <div className="h-screen md:text-md bg-gradient-to-br from-blue-900 to-blue-300 dark:from-black dark:to-slate-950">
         <div className="md:max-w-[1200px] md:mx-auto">
           <Header />
-          <div className="px-8">
+          <div className="px-6 md:px-8">
             <div className="py-2 my-8 flex flex-col items-center justify-center">
               <h1 className="text-5xl font-semibold">Daniel Evans</h1>
               <p className="text-lg my-2">Data engineer | danieljevans168@gmail.com</p>
             </div>
             <div>
-              <p>Hello! My name is Daniel and this is my personal website. I am a data engineer currently based in Chester, UK with 3 years of experience working in a agile development team</p>
+              <p>Hello! My name is Daniel and this is my personal website. I am a data engineer currently based in Chester, UK with 3 years experience working in a professional agile development team.</p>
             </div>
+
             {/* Work section */}
             <div className="flex flex-col my-4">
               <h1 className="text-xl font-semibold">Work</h1>
-              {/* Bank of America */}
-              <div className="mx-1 flex flex-row justify-between">
-                <h2 className="text-lg"> Bank of America</h2>
-              </div>
-              {/* Tesco */}
-              <div>
 
+              {/* Bank of America */}
+              <div className="mx-1 my-4">
+                <h2 className="text-lg"> Bank of America</h2>
+                <p>2022 - Present<span className="mx-4">|</span>Data Engineer</p>
+              </div>
+
+              {/* Tesco */}
+              <div className="mx-1 my-4">
+                <h2 className="text-lg"> Tesco</h2>
+                <p>2020 - 2022<span className="mx-4">|</span>Customer Assitant & Shift Leader</p>
               </div>
             </div>
+
             {/* Eductaion section */}
-            <div className="flex flex-col items-center">
-              Education
+            <div className="flex flex-col my-4">
+              <h1 className="text-xl font-semibold">Education</h1>
               {
                 education_data.map((data, index) => {
                   return <EducationItem key={index} {...data} />
                 })
               }
+            </div>
+
+            {/* Technologies section */}
+            <div className="flex flex-col my-4">
+              <h1 className="text-xl font-semibold">My Tech Stack</h1>
+              <div className="my-8 flex flex-row justify-evenly">
+                <FaPython size={25} />
+                <DiMsqlServer size={25} />
+                <SiTypescript size={25} />
+                <FaHtml5 size={25} />
+              </div>
             </div>
 
           </div>
