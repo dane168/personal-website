ef99e1fb336d0b9262dd410aec45a89d17935d2c
dane168 <danieljevans168@gmail.com>
2025-02-28 20:59:30 +0000

    header fixed and mobile format added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Header from "@/components/Header";
 import EducationItem from "../components/education_item";
+import { promises as fs } from 'fs';
+import Image from "next/image";
 
 export type EducationData = {
   school: string;
@@ -9,29 +11,47 @@ export type EducationData = {
   location: string;
 }
 
-export default function Home() {
-  const education_data = [{
-    school: "University of Leicester",
-    degree: "MPhys in Physics with Space Science",
-    start_date: "2018",
-    end_date: "2022",
-    location: "Leicester, UK",
-  }]
+export default async function Home() {
+  const file = await fs.readFile(process.cwd() + '/data/education.json', 'utf8');
+  const education_data: EducationData[] = JSON.parse(file)
 
   return (
     <>
-      <div className="h-screen bg-gradient-to-br from-blue-900 to-blue-300 dark:bg-gradient-to-r dark:from-slate-900 dark:to-slate-800">
-        <Header />
-        <div className="flex flex-col items-center justify-center py-2">
-          <h1 className="text-4xl font-semibold">Daniel Evans</h1>
-          <p>Data engineer</p>
-        </div>
-        <div className="flex flex-col items-center">
-          {
-            education_data.map((data, index) => {
-              return <EducationItem key={index} {...data} />
-            })
-          }
+      <div className="h-screen md:text-md bg-gradient-to-br from-blue-900 to-blue-300 dark:bg-gradient-to-r dark:from-black dark:to-slate-950">
+        <div className="md:max-w-[1200px] md:mx-auto">
+          <Header />
+          <div className="px-8">
+            <div className="py-2 my-8 flex flex-col items-center justify-center">
+              <h1 className="text-5xl font-semibold">Daniel Evans</h1>
+              <p className="text-lg my-2">Data engineer | danieljevans168@gmail.com</p>
+            </div>
+            <div>
+              <p>Hello! My name is Daniel and this is my personal website. I am a data engineer currently based in Chester, UK with 3 years of experience working in a agile development team</p>
+            </div>
+            {/* Work section */}
+            <div className="flex flex-col my-4">
+              <h1 className="text-xl font-semibold">Work</h1>
+              {/* Bank of America */}
+              <div className="mx-1 flex flex-row justify-between">
+                <h2 className="text-lg"> Bank of America</h2>
+              </div>
+              {/* Tesco */}
+              <div>
+
+              </div>
+            </div>
+            {/* Eductaion section */}
+            <div className="flex flex-col items-center">
+              Education
+              {
+                education_data.map((data, index) => {
+                  return <EducationItem key={index} {...data} />
+                })
+              }
+            </div>
+
+          </div>
+
         </div>
 
       </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,33 +2,29 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { ModeToggle } from './dark-toggle'
-import { FaGithub } from 'react-icons/fa'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
 const Header = () => {
   return (
     <div className=''>
-      <nav className='py-4 mx-auto max-w-[1200px] flex flex-row justify-between items-center '>
+      <nav className='py-4 flex flex-row justify-between items-center '>
         <div>
-          <div className='mx-2 rounded-full overflow-hidden'>
-            <Image src={"/1000006170.jpg"} width={100} height={100} alt='icon' />
+          <div className='mx-2 rounded-3xl overflow-hidden'>
+            <Image src={"/1659083620630.jpeg"} width={70} height={70} alt='icon' />
           </div>
         </div>
         <div className='flex flex-row'>
-          <div className='mx-4 text-black dark:text-white flex flex-col justify-center items-center'>
-            <Link href="/" className='align-middle text-lg'>Experience</Link>
+          <div className='mx-2 text-black dark:text-white flex flex-col justify-center items-center'>
+            <Link href={"https://www.linkedin.com/in/daniel-evans-3b4977176"} >
+              <FaLinkedin size={30} />
+            </Link>
           </div>
-          <div className='mx-4 text-black dark:text-white flex flex-col justify-center items-center'>
-            <Link href="/" className='align-middle text-lg'>Education</Link>
-          </div>
-          <div className='mx-4 text-black dark:text-white flex flex-col justify-center items-center'>
-            <Link href="/" className='align-middle text-lg'>Skills</Link>
-          </div>
-          <div className='mx-4 text-black dark:text-white flex flex-col justify-center items-center'>
+          <div className='mx-2 text-black dark:text-white flex flex-col justify-center items-center'>
             <Link href={"https://github.com/dane168/personal-website"} >
               <FaGithub size={30} />
             </Link>
           </div>
-          <div className='me-2'>
+          <div className='mx-2'>
             <ModeToggle />
           </div>
         </div>
